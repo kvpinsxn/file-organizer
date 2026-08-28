97af1168283c278ad65a0a998209f3b11645f981
Kacper Kaleta <kacper.kaleta734@gmail.com>
2026-08-28 16:58:30 +0200

    Added json log

--- organizer.py
+++ organizer.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import sys
+import json
 
 if len(sys.argv) != 2:
     print("Wrong numbers of arguments")
@@ -10,6 +11,12 @@ path = sys.argv[1]
 
 dir_list = os.listdir(path=path)
 
+raport = {
+    "folder": path,
+    "moved_files": {},
+    "total": 0
+}
+
 for i in dir_list:
     name, ext = os.path.splitext(i)
     file = os.path.join(path, i)
@@ -18,9 +25,15 @@ for i in dir_list:
             dest_no = os.path.join(path, "no_extension")
             os.makedirs(dest_no, exist_ok=True)
             shutil.move(file, dest_no)
+            category = "no_extension"
         else:
             dest = os.path.join(path, ext)
             os.makedirs(dest, exist_ok=True)
             shutil.move(file, dest)
-    else:
-        pass
+            category = ext
+
+        raport["moved_files"][category] = raport["moved_files"].get(category, 0) + 1
+        raport["total"] += 1
+
+with open("raport.json", "w") as raport_file:
+    json.dump(raport, raport_file, indent=2)
